7b12ce7dc67fe349082f937321fec71329882d40
Produpro <136916627+Produpro@users.noreply.github.com>
2024-01-16 20:05:44 +0000

    prueba

--- src/app.py
+++ src/app.py
@@ -1,4 +1,27 @@
-from utils import db_connect
-engine = db_connect()
+from flask import Flask, request, render_template
+from pickle import load
 
-# your code here
+app = Flask(__name__)
+model = load(open("/workspaces/NEW_flask/models/decision_tree_classifier_default_42.sav", "rb"))
+class_dict = {
+    "0": "Iris setosa",
+    "1": "Iris versicolor",
+    "2": "Iris virginica"
+}
+
+@app.route("/", methods = ["GET", "POST"])
+def index():
+    if request.method == "POST":
+        
+        val1 = float(request.form["val1"])
+        val2 = float(request.form["val2"])
+        val3 = float(request.form["val3"])
+        val4 = float(request.form["val4"])
+        
+        data = [[val1, val2, val3, val4]]
+        prediction = str(model.predict(data)[0])
+        pred_class = class_dict[prediction]
+    else:
+        pred_class = None
+    
+    return render_template("index.html", prediction = pred_class)
